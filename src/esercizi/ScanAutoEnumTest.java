d260a6f57fa0100364d143ef349bfe0d0757f63f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-02-06 00:10:14 +0100

    existsProducerTest() method done - JUnit

--- src/esercizi/ScanAutoEnumTest.java
+++ src/esercizi/ScanAutoEnumTest.java
@@ -3,6 +3,7 @@ package esercizi;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ScanAutoEnumTest {
@@ -10,14 +11,29 @@ class ScanAutoEnumTest {
 	static ScanAutoEnum scanAutoEnum;
 	static String autoBrandsFileName = "src/text_files/auto-brands.txt";
 
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//		scanAutoEnum = new ScanAutoEnum(autoBrandsFileName);
-//	}
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		scanAutoEnum = new ScanAutoEnum(autoBrandsFileName);
+	}
 
 	@Test
-	void testAutoProducers() {
-		scanAutoEnum = new ScanAutoEnum(autoBrandsFileName);
+	void existsProducerTest() {
+		assertTrue(scanAutoEnum.existsProducer("Stellantis"));
+		assertTrue(scanAutoEnum.existsProducer("stellantis"));
+		assertTrue(scanAutoEnum.existsProducer("STELLANTIS"));
+		assertTrue(scanAutoEnum.existsProducer("Audi"));
+		assertTrue(scanAutoEnum.existsProducer("audi"));
+		assertTrue(scanAutoEnum.existsProducer("AUDI"));
+		assertTrue(scanAutoEnum.existsProducer("Tesla"));
+		assertTrue(scanAutoEnum.existsProducer("tesla"));
+		assertTrue(scanAutoEnum.existsProducer("TESLA"));
+		assertFalse(scanAutoEnum.existsProducer("Bmw"));
+		assertFalse(scanAutoEnum.existsProducer("bmw"));
+		assertFalse(scanAutoEnum.existsProducer("BMW"));
 	}
 
 }
